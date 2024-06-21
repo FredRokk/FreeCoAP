15259eeec86aeb9a6eab87621753b500dfa15f82
Frederik Rokkjær <fbr@je-electronic.dk>
2024-06-21 08:46:44 +0200

    gen_certs.sh has been altered to make 10 certificates and keys for clients, meant for parrallel client tests

--- certs/gen_certs.sh
+++ certs/gen_certs.sh
@@ -78,24 +78,232 @@ rmcond client_cert.pem
 #          --load-privkey root_client_privkey.pem
 # echo ""
 
+# Certificate 1
 echo "----------------------------------------"
-echo "Client Private Key"
+echo "Client Private Key 1"
 echo "----------------------------------------"
 certtool --generate-privkey \
          --ecc \
          --curve secp256r1 \
-         --outfile client_privkey.pem
+         --outfile client_privkey_1.pem
 echo ""
 
 echo "----------------------------------------"
-echo "Client Certificate"
+echo "Client Certificate 1"
 echo "----------------------------------------"
 certtool --generate-certificate \
          --ecc \
          --curve secp256r1 \
          --template client_template.txt \
-         --outfile client_cert.pem \
-         --load-privkey client_privkey.pem \
+         --outfile client_cert_1.pem \
+         --load-privkey client_privkey_1.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 2
+echo "----------------------------------------"
+echo "Client Private Key 2"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_2.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 2"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_2.pem \
+         --load-privkey client_privkey_2.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 3
+echo "----------------------------------------"
+echo "Client Private Key 3"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_3.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 3"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_3.pem \
+         --load-privkey client_privkey_3.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 4
+echo "----------------------------------------"
+echo "Client Private Key 4"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_4.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 4"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_4.pem \
+         --load-privkey client_privkey_4.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 5
+echo "----------------------------------------"
+echo "Client Private Key 5"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_5.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 5"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_5.pem \
+         --load-privkey client_privkey_5.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 6
+echo "----------------------------------------"
+echo "Client Private Key 6"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_6.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 6"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_6.pem \
+         --load-privkey client_privkey_6.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 7
+echo "----------------------------------------"
+echo "Client Private Key 7"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_7.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 7"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_7.pem \
+         --load-privkey client_privkey_7.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 8
+echo "----------------------------------------"
+echo "Client Private Key 8"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_8.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 8"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_8.pem \
+         --load-privkey client_privkey_8.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 9
+echo "----------------------------------------"
+echo "Client Private Key 9"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_9.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 9"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_9.pem \
+         --load-privkey client_privkey_9.pem \
+         --load-ca-certificate root_client_cert.pem \
+         --load-ca-privkey root_client_privkey.pem
+echo ""
+
+# Certificate 10
+echo "----------------------------------------"
+echo "Client Private Key 10"
+echo "----------------------------------------"
+certtool --generate-privkey \
+         --ecc \
+         --curve secp256r1 \
+         --outfile client_privkey_10.pem
+echo ""
+
+echo "----------------------------------------"
+echo "Client Certificate 10"
+echo "----------------------------------------"
+certtool --generate-certificate \
+         --ecc \
+         --curve secp256r1 \
+         --template client_template.txt \
+         --outfile client_cert_10.pem \
+         --load-privkey client_privkey_10.pem \
          --load-ca-certificate root_client_cert.pem \
          --load-ca-privkey root_client_privkey.pem
 echo ""
